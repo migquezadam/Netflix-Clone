ea27cf603ceb3d52c36141c089a12352d00e884c
Miguel Quezada <mig.aquezada@gmail.com>
2022-07-28 19:49:07 -0400

    Initial Commit

---
+++ Netflix Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix Clone
+//
+//  Created by Miguel Quezada on 28-07-22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
